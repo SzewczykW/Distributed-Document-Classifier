34fd30e18eceae4336c1ba038109bd9d363aa01a
SzewczykW <1szewczyk@fis.agh.edu.pl>
2025-05-03 02:07:05 +0200

    Update make run

--- make/run.mk
+++ make/run.mk
@@ -1,18 +1,17 @@
-MPI_FLAGS ?= -np 4
-RUN_FLAGS ?= --input ./data --output result.txt
+MPI_FLAGS ?= -f nodes -n 16
+RUN_FLAGS ?= --input ./example/input/ --output ./example/output/result.txt
 
 run: $(TARGET)
-	LD_LIBRARY_PATH=$(MPICH_TARGET_DIR)/lib \
-	mpirun $(MPI_FLAGS) $(TARGET) $(RUN_FLAGS)
+	LD_LIBRARY_PATH=$(MPICH_TARGET_DIR)/lib MPIR_CVAR_ENABLE_GPU=0 mpiexec $(MPI_FLAGS) $(TARGET) $(RUN_FLAGS)
 
 run-help:
 	@echo ""
 	@echo "make run:"
 	@echo "  Run the program using mpirun"
 	@echo "  You can pass additional flags:"
-	@echo "    MPI_FLAGS='-np 4'          (number of processes)"
-	@echo "    RUN_FLAGS='--input ./data --output result.txt'"
+	@echo "    MPI_FLAGS='-f nodes -n 16'"
+	@echo "    RUN_FLAGS='--input ./example/input/ --output ./example/output/result.txt'"
 	@echo "  Example:"
-	@echo "    make run MPI_FLAGS='-np 8' RUN_FLAGS='--input in --output out.txt'"
+	@echo "    make run MPI_FLAGS='-f nodes -n 8' RUN_FLAGS='--input in_dir --output out.txt'"
 	@echo ""
 
